6c1e255375e910965aa1d559a15d0449a3f783ab
Yovie Fesya Pratama <yoviefp@gmail.com>
2024-03-07 14:53:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <iframe
+      src="https://codesandbox.io/embed/6s2dpw?view=Editor+%2B+Preview&module=%2Fsrc%2Findex.ts"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: 0,
+        borderRadius: "4px",
+        overflow: "hidden",
+      }}
+      title="vigilant-swartz"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  );
+};
+
+export default App;
